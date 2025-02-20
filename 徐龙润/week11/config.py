29f53752dd147ec2669719f64d6646ab98e19469
xlr134 <138568644+xlr134@users.noreply.github.com>
2025-02-20 21:32:42 +0800

    Create config.py

---
+++ 徐龙润/week11/config.py
@@ -0,0 +1,11 @@
+
+Config = {
+    "bert_path" : "",
+    "data_path" : r"C:\Users\58353\Desktop\Depp leraing\第十一周 大模型相关内容第一讲\week11 大语言模型相关第一讲\data\tag_news.json",
+    "max_seq_len" : 300,
+    "batch_size" : 32,
+    "epoch":40,
+    "check_point_path":"check_point",
+    "optimizer":"adam",
+    "learning_rate":1e-3,
+}
